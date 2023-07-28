32c5ee0647dc085ad7d2c39f153b4869c74832a8
Antonio Barbosa <acrb1108@gmail.com>
2023-07-28 17:51:28 -0300

    Atualização 02 aula projeto 5

--- ProjetoAula05/Entities/Funcionario.cs
+++ ProjetoAula05/Entities/Funcionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoAula05.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,41 @@ using System.Threading.Tasks;
 
 namespace ProjetoAula05.Entities
 {
+    /// <summary>
+    /// Classe de entidade
+    /// </summary>
     public class Funcionario
     {
-        public Guid IdFuncionario { get; set; }
-        public string Nome { get; set; }
-        public string Matricula { get; set; }
-        public DateTime DataAdmissao { get; set; }
+        #region Atributos
 
+        private Guid _id;
+        private string _nome;
+        private string _cpf;
+        private string _matricula;
+        private DateTime _dataAdmissao;
+        private TipoContratacao _Tipo;
+
+        #endregion
+
+        #region Propriedades
+
+        public Guid Id
+        {
+            get => _id;
+            set { _id = value; }
+        }
+
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+
+            }
+        }
+
+
+
+        #endregion
     }
 }
